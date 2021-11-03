5826c1e124953b1868803de205e100e6e02c2dda
stengah <deepsghimire1@gmail.com>
2021-11-03 15:16:29 +0545

    added addresses and ascii

--- src/main.rs
+++ src/main.rs
@@ -13,19 +13,33 @@ fn to_hex(byte: u8) -> String {
     format!("{:02x}", byte)
 }
 
+fn line_view(bytes: &[u8], byte_count: usize) -> String {
+    let hexes: String = bytes.iter().map(|b| format!("{} ", to_hex(*b))).collect();
+    let address = format!("0x{:08x?}", byte_count);
+    let asciis: String = bytes
+        .iter()
+        .map(|b| {
+            if !b.is_ascii_control() {
+                *b as char
+            } else {
+                '.'
+            }
+        })
+        .collect();
+    format!("{} | {} | {} \n", address, hexes, asciis)
+}
+
 fn hex_view(bytes: &[u8]) {
     let mut writer = BufWriter::new(stdout());
+    let elem_per_row = 16;
 
-    let mut column = 0;
-    for byte in bytes {
-        if column % 16 != 0 {
-            write!(writer, "{} ", to_hex(*byte));
-        } else {
-            write!(writer, "{}\n", to_hex(*byte));
-        }
-
-        column += 1;
+    let mut start = 0;
+    while start + elem_per_row < bytes.len() {
+        let line = line_view(&bytes[start..start + elem_per_row], start);
+        write!(writer, "{}", line);
+        start += elem_per_row;
     }
+    write!(writer, "{}", line_view(&bytes[start..], start));
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
